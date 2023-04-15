f2fb7be502484a629061dd781a1adfa3cba2e58d
xyz <rajsharma1234@gmail.com>
2023-04-15 19:30:33 +0530

    15.04.23

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const authValidator = require("../validators/auth");
-
 const runValidation = require("../validators/validationResult");
 const authController = require("../controllers/auth");
 
