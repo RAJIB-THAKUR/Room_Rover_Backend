29648d14496d929cd7868ea924a0e6af1d7556d2
xyz <rajsharma1234@gmail.com>
2023-04-20 22:48:39 +0530

    20/04/23

--- API_List.js
+++ API_List.js
@@ -1,76 +1,76 @@
-//-------------routes auth.js-----------
-1)post
-https://room-rover-app-backend-mern.onrender.com/api/register
+// //-------------routes auth.js-----------
+// 1)post
+// https://room-rover-app-backend-mern.onrender.com/api/register
 
-2)post
-https://room-rover-app-backend-mern.onrender.com/api/login
+// 2)post
+// https://room-rover-app-backend-mern.onrender.com/api/login
 
-3)put
-https://room-rover-app-backend-mern.onrender.com/api/generateOTP
+// 3)put
+// https://room-rover-app-backend-mern.onrender.com/api/generateOTP
 
-4)post
-https://room-rover-app-backend-mern.onrender.com/api/verifyOTP
+// 4)post
+// https://room-rover-app-backend-mern.onrender.com/api/verifyOTP
 
-5)put
-https://room-rover-app-backend-mern.onrender.com/api/updatePassword
+// 5)put
+// https://room-rover-app-backend-mern.onrender.com/api/updatePassword
 
-------------------------------------------------------------------------------------------------------------------------------------------
-
-
-//-------------routes user.js-----------
-1)get
-https://room-rover-app-backend-mern.onrender.com/api/user_booking_Details
-
-2)get
-https://room-rover-app-backend-mern.onrender.com/api/user_building_Details
+// ------------------------------------------------------------------------------------------------------------------------------------------
 
 
-------------------------------------------------------------------------------------------------------------------------------------------
+// //-------------routes user.js-----------
+// 1)get
+// https://room-rover-app-backend-mern.onrender.com/api/user_booking_Details
+
+// 2)get
+// https://room-rover-app-backend-mern.onrender.com/api/user_building_Details
 
 
-
-//-------------routes seller.js-----------
-1)get
-https://room-rover-app-backend-mern.onrender.com/api/seller_buildingDetails_type_City
-
-2)get
-https://room-rover-app-backend-mern.onrender.com/api/booking_Details
-
-3)put
-https://room-rover-app-backend-mern.onrender.com/api/seller_update_RoomCount
-
-
-------------------------------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
-//-------------routes booking.js-----------
-1)post
-https://room-rover-app-backend-mern.onrender.com/api/bookRoom
+// //-------------routes seller.js-----------
+// 1)get
+// https://room-rover-app-backend-mern.onrender.com/api/seller_buildingDetails_type_City
 
-2)put
-https://room-rover-app-backend-mern.onrender.com/api/cancelBooking
+// 2)get
+// https://room-rover-app-backend-mern.onrender.com/api/booking_Details
 
-3)put
-https://room-rover-app-backend-mern.onrender.com/api/checkOut_User
-
-
-
-------------------------------------------------------------------------------------------------------------------------------------------
+// 3)put
+// https://room-rover-app-backend-mern.onrender.com/api/seller_update_RoomCount
 
 
-//-------------routes building.js-----------
-1)post
-https://room-rover-app-backend-mern.onrender.com/api/addBuilding
+// ------------------------------------------------------------------------------------------------------------------------------------------
 
-2)delete
-https://room-rover-app-backend-mern.onrender.com/api/deleteBuilding
 
-3)get
-https://room-rover-app-backend-mern.onrender.com/api/allCities_roomCount_minCost
 
-4)get
-https://room-rover-app-backend-mern.onrender.com/api/allBuildingTypes_roomCount_minCost
+// //-------------routes booking.js-----------
+// 1)post
+// https://room-rover-app-backend-mern.onrender.com/api/bookRoom
 
-5)get
-https://room-rover-app-backend-mern.onrender.com/api/buildingDetails_Type_City_wise
+// 2)put
+// https://room-rover-app-backend-mern.onrender.com/api/cancelBooking
+
+// 3)put
+// https://room-rover-app-backend-mern.onrender.com/api/checkOut_User
+
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// //-------------routes building.js-----------
+// 1)post
+// https://room-rover-app-backend-mern.onrender.com/api/addBuilding
+
+// 2)delete
+// https://room-rover-app-backend-mern.onrender.com/api/deleteBuilding
+
+// 3)get
+// https://room-rover-app-backend-mern.onrender.com/api/allCities_roomCount_minCost
+
+// 4)get
+// https://room-rover-app-backend-mern.onrender.com/api/allBuildingTypes_roomCount_minCost
+
+// 5)get
+// https://room-rover-app-backend-mern.onrender.com/api/buildingDetails_Type_City_wise
