e0d7f275bf39d6c1a221ec1c14de0dbf944b1789
xyz <rajsharma1234@gmail.com>
2023-04-22 14:58:45 +0530

    22/04/23

--- routes/user.js
+++ routes/user.js
@@ -6,22 +6,13 @@ const router = express.Router();
 const userController = require("../controllers/user");
 
 //-------------------------------------ROUTES-----------------------
-//ROUTE-1 : 
-router.get(
-  "/user_booking_Details",
-  userController.user_booking_Details
-);
+//ROUTE-1 :
+router.get("/user_booking_Details", userController.user_booking_Details);
 
 //ROUTE-2 : user gets details of building with building_id
-router.get(
-    "/user_building_Details",
-    userController.user_building_Details
-  );
+router.get("/user_building_Details", userController.user_building_Details);
 
-  //ROUTE:3 : fetch user or seller profile details
-router.get(
-  "/user_seller_profile",
-  userController.user_seller_profile
-);
+//ROUTE:2 : fetch user or seller profile details
+router.get("/user_seller_profile", userController.user_seller_profile);
 
 module.exports = router;
