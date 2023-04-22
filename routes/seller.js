85a6c455dcba57b1d4b5fa59b46f57b2a652dbdb
xyz <rajsharma1234@gmail.com>
2023-04-22 15:15:24 +0530

    22/04/23

--- routes/seller.js
+++ routes/seller.js
@@ -7,18 +7,17 @@ const sellerController = require("../controllers/seller");
 
 //-------------------------------------ROUTES-----------------------
 //ROUTE-1 : All Building details for particular seller buildingType wise or city wise or both (common API for three queries)
-router.get(
+router.post(
   "/seller_buildingDetails_type_City",
   sellerController.seller_buildingDetails_type_City
 );
 //ROUTE:2 : seller can get Details of users in particular building
-router.get("/booking_Details", sellerController.seller_booking_Details);
+router.post("/booking_Details", sellerController.seller_booking_Details);
 
 //ROUTE:3 : seller can update RoomCount in particular building
 router.put(
   "/seller_update_RoomCount",
   sellerController.seller_update_RoomCount
 );
-
 
 module.exports = router;
